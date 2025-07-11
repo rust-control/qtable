d6ff94a09efe6b5cb4831336a60b8316a52361a6
kanarus <kanarus786@gmail.com>
2025-07-11 09:19:15 +0900

    `{Action, State}::new_on: _ -> Result`

--- src/action.rs
+++ src/action.rs
@@ -9,9 +9,29 @@ impl std::ops::Deref for Action {
     }
 }
 
+#[derive(Debug)]
+pub enum ActionError {
+    InvalidActionIndex { given: usize, max: usize },
+}
+impl std::error::Error for ActionError {}
+impl std::fmt::Display for ActionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ActionError::InvalidActionIndex { given, max } => {
+                write!(f, "Invalid action index: {} (max: {})", given, max)
+            }
+        }
+    }
+}
+
 impl Action {
-    pub fn new_on(qtable: &crate::QTable, index: usize) -> Option<Self> {
-        (index < qtable.action_size()).then_some(Self(index))
+    pub fn new_on(qtable: &crate::QTable, index: usize) -> Result<Self, ActionError> {
+        (index < qtable.action_size())
+            .then_some(Self(index))
+            .ok_or(ActionError::InvalidActionIndex {
+                given: index,
+                max: qtable.action_size(),
+            })
     }
 
     pub fn index(&self) -> usize {
--- src/state.rs
+++ src/state.rs
@@ -9,9 +9,29 @@ impl std::ops::Deref for State {
     }
 }
 
+#[derive(Debug)]
+pub enum StateError {
+    InvalidStateIndex { given: usize, max: usize },
+}
+impl std::error::Error for StateError {}
+impl std::fmt::Display for StateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            StateError::InvalidStateIndex { given, max } => {
+                write!(f, "Invalid state index: {} (max: {})", given, max)
+            }
+        }
+    }
+}
+
 impl State {
-    pub fn new_on(qtable: &crate::QTable, index: usize) -> Option<Self> {
-        (index < qtable.state_size()).then_some(Self(index))
+    pub fn new_on(qtable: &crate::QTable, index: usize) -> Result<Self, StateError> {
+        (index < qtable.state_size())
+            .then_some(Self(index))
+            .ok_or(StateError::InvalidStateIndex {
+                given: index,
+                max: qtable.state_size(),
+            })
     }
 
     pub fn index(&self) -> usize {
